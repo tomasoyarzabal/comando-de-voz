cea17d4b2e464e1120c324086186ca38688c5870
tomasoyarzabal <tomasoyarzabal135@gmail.com>
2022-09-05 17:25:56 -0300

    voz

---
+++ voz/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace voz
+{
+    partial class Frmvoz
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(477, 95);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(88, 23);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "iniciar";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(583, 95);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(98, 23);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "cancelar";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(669, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Frmvoz
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 171);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Frmvoz";
+            this.Text = "Formulario";
+            this.Load += new System.EventHandler(this.Frmvoz_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ voz/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace voz
+{
+    public partial class Frmvoz : Form
+        
+    {
+        SpeechRecognitionEngine oEscucha = new SpeechRecognitionEngine();
+        public Frmvoz()
+        {
+            InitializeComponent();
+        }
+
+        private void Frmvoz_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            oEscucha.SetInputToDefaultAudioDevice();
+            oEscucha.LoadGrammar(new DictationGrammar());
+            oEscucha.SpeechRecognized += Detection;
+            oEscucha.RecognizeAsync(RecognizeMode.Multiple);
+        }
+        private void Detection(object sender, SpeechRecognizedEventArgs e)
+        {
+            textBox1.Text = e.Result.Text;
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            oEscucha.RecognizeAsyncStop();
+        }
+    }
+}
